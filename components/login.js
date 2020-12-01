855a37ed1d8ba3c8fc8cd1fbd78fd31993b09658
mertoz41 <mertozka41@gmail.com>
2020-12-01 11:57:45 -0500

    simple login functionality

---
+++ components/login.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+
+
+export default function Login(){
+
+
+    const [username, usernameChange] = React.useState()
+    const [password, passwordChange] = React.useState()
+
+
+    const fetchUser = () => {
+        let obj = {
+            username: username,
+            password: password 
+        }
+        fetch('http://localhost:3000/login', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            if (resp.user){
+                console.log(resp.user)
+            } else {
+                Alert.alert(resp.message)
+            }
+    
+        })
+    }
+
+
+
+
+        return(
+            <View>
+                <Text>Username:</Text>
+                <TextInput onChangeText={text => usernameChange(text)} value={username} autoCapitalize='none'/>
+                <Text>Password:</Text>
+                <TextInput secureTextEntry={true} onChangeText={text => passwordChange(text)} value={password} />
+                <Button title="LOGIN" onPress={() => fetchUser()} />
+            </View>
+        )
+    }
+
+
+
